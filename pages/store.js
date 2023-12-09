faf64bd1a5c7f611936b430aeed07db83e5f2e63
OswaldCardoz <145090621+OswaldCardoz@users.noreply.github.com>
2023-12-09 19:43:26 +0530

    updated

--- pages/store.js
+++ pages/store.js
@@ -1,10 +1,22 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 function Store() {
+  const router =useRouter();
+  useEffect(()=>{
+   if( localStorage.getItem('loginStatus')!=='true'){
+    router.push('/login');
+   }
+  },[])
+
+  const handelLogout=()=>{
+    localStorage.setItem('loginStatus','false');
+    router.push('/login');
+  }
+
   return (
     <div className='store'>
       <h2>Welcome to the store!</h2>
-      <button className='logout-btn'>Logout</button>
+      <button className='logout-btn'onClick={handelLogout}>Logout</button>
     </div>
   );
 }
